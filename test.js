c717854c9345cc935585285cc9414f378dc81e58
Kevin Kihara <kevoh@nexttech>
2020-07-18 15:04:24 +0300

    tested the javascript 100 days of code challenge 1

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2==0.3);
+console.log(isNaN('javascript'));
+console.log(isNaN('78'));
+console.log("0 || 1 = "+(0 || 1));
+console.log("1 || 2 = "+(1 || 2));
+console.log("0 && 1 = "+(0 && 1)); 
+console.log("1 && 2 = "+(1 && 2));
